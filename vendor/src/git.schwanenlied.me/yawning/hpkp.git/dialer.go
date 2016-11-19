d1bc3b730b6d07b9b025b889cb42ca342ce825ae
Yawning Angel <yawning@schwanenlied.me>
2016-11-19 19:37:58 +0000

    Update the hpkp library. #31
    The `tls.Config` deep copy routine I added to my fork was attempting to
    copy fields that were added in Go 1.7.  This now has been addressed.

--- vendor/src/git.schwanenlied.me/yawning/hpkp.git/dialer.go
+++ vendor/src/git.schwanenlied.me/yawning/hpkp.git/dialer.go
@@ -124,29 +124,3 @@ func newPinDialer(s StorageReader, r PinFailureReporter, pinOnly bool, defaultTL
 		return dialTLS(network, addr, defaultTLSConfig)
 	}
 }
-
-func cloneTLSConfig(cfg *tls.Config) *tls.Config {
-	return &tls.Config{
-		Rand:                        cfg.Rand,
-		Time:                        cfg.Time,
-		Certificates:                cfg.Certificates,
-		NameToCertificate:           cfg.NameToCertificate,
-		GetCertificate:              cfg.GetCertificate,
-		RootCAs:                     cfg.RootCAs,
-		NextProtos:                  cfg.NextProtos,
-		ServerName:                  cfg.ServerName,
-		ClientAuth:                  cfg.ClientAuth,
-		ClientCAs:                   cfg.ClientCAs,
-		InsecureSkipVerify:          cfg.InsecureSkipVerify,
-		CipherSuites:                cfg.CipherSuites,
-		PreferServerCipherSuites:    cfg.PreferServerCipherSuites,
-		SessionTicketsDisabled:      cfg.SessionTicketsDisabled,
-		SessionTicketKey:            cfg.SessionTicketKey,
-		ClientSessionCache:          cfg.ClientSessionCache,
-		MinVersion:                  cfg.MinVersion,
-		MaxVersion:                  cfg.MaxVersion,
-		CurvePreferences:            cfg.CurvePreferences,
-		DynamicRecordSizingDisabled: cfg.DynamicRecordSizingDisabled,
-		Renegotiation:               cfg.Renegotiation,
-	}
-}
